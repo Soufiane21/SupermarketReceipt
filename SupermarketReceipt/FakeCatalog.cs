2d47ac373674729b068d763d87670f85e1048112
SWORKS <45871235+Soufiane21@users.noreply.github.com>
2025-11-02 22:10:40 +0100

    Add comments to 5 random files

--- SupermarketReceipt/FakeCatalog.cs
+++ SupermarketReceipt/FakeCatalog.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 namespace SupermarketReceiptTests
 {
+    // Fake catalog implementation for testing purposes
     public class FakeCatalog : ISupermarketCatalog
     {
         private readonly IDictionary<string, double> _prices = new Dictionary<string, double>();
